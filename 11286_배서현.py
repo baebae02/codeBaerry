81d35cb37eeb5fc1d522156f24f443ef17f96f22
Seo Hyeon Bae <76519535+baebae02@users.noreply.github.com>
2022-07-18 22:46:29 +0900

    Create 11286_배서현.py

---
+++ 11286_배서현.py
@@ -0,0 +1,15 @@
+import sys
+import heapq
+
+heap = []
+N = int(sys.stdin.readline())
+
+for _ in range(N):
+	num = int(sys.stdin.readline())
+	if num == 0:
+		if len(heap) == 0:
+			print(0)
+		else:
+			print(heapq.heappop(heap)[1])
+	else:
+		heapq.heappush(heap, (abs(num), num))
