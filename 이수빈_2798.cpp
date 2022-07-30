cf80b7405f2b9fcbbd680eafdf11ce448e5d1032
subin <81912246+200516bb@users.noreply.github.com>
2022-07-30 22:51:25 +0900

    Update 이수빈_2798.cpp

--- 이수빈_2798.cpp
+++ 이수빈_2798.cpp
@@ -13,11 +13,9 @@ int main(){
         v.push_back(temp);
     }
 
-    for(int i = 0; i<N; i++){
-        for(int j = i+1; j<N; j++){
-            if(i==j) continue;
+    for(int i = 0; i<N-2; i++){
+        for(int j = i+1; j<N-1; j++){
             for(int k = j+1; k<N; k++){
-                if(j==k) continue;
                 result = v[i] + v[j] + v[k];
                 if(result > M) continue;
                 if(result > max) max = result;
